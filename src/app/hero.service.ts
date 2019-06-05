d29c0c26d3b255d5faaf51f55c3d8a14113d898c
AdityaShD <adityasharma965@gmail.com>
2019-06-06 00:36:07 +0530

    added httpclient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import {Observable, of} from 'rxjs';
-import { MessageService} from "./message.service";
+import { MessageService} from './message.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched Heroes');
-    return of (HEROES);
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient,
+    ) { }
+    private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
+    }
+
 }
